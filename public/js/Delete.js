76d569b8112bc059e806d7286c6b6f23b78492f9
Jared Windover <jaredwindover@gmail.com>
2015-03-09 14:09:44 -0400

    redirect no longer needed, and some comments in the others

--- public/js/Delete.js
+++ public/js/Delete.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
+  // Override delete action to allow for confirmation
   $("#delete").click(function(event){
     event.preventDefault();
     if (window.confirm("Are you sure you want to delete this note?")) {
-      $.ajax({
-	url: $("#delete").attr("href"),
-	type: 'DELETE',
-	success: function(result) {
-	  window.location.href='/';
-	}
-      });
+      $.ajax(
+	{
+	  url: $("#delete").attr("href"),
+	  type: 'DELETE',
+	  success: function(result) {
+	    window.location.href='/';
+	  }
+	});
     }
     else {
       return false;
--- public/js/Edit.js
+++ public/js/Edit.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  form = $("#form1");
-  form.submit(function() {
+  //Override submit action to redirect after
+  $("#form1").submit(function() {
     var title = $("#title").val();
     var content = $("#content").val();
-    $.post(form.attr("action"),
+    // Use form's action as url
+    $.post($(this).attr("action"),
 	   {
 	     title:title,
 	     content:content
@@ -12,12 +13,15 @@ $(document).ready(function() {
 	     window.location.href='/Notes/'+title.replace(/ /g,'');
 	   });
     return false;
-  })
+  });
+  //On content change, update mathjax
   $("#content")
       .bind('input propertychange',function(){
 	$("#Preview").html($(this).val());
 	MathJax.Hub.Queue(["Typeset",MathJax.Hub,"Preview"]);
       });
+
+  //Set up preview on load
   $("#Preview").html($("#content").val());
   MathJax.Hub.Queue(["Typeset",MathJax.Hub,"Preview"]);
-})
+});
