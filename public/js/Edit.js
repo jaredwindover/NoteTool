e7f38007c0a282bef8a196446944a26cdd11d380
Jared Windover <jaredwindover@gmail.com>
2015-03-15 23:20:59 -0400

    Including mathjax example code for live previewing so that updates aren't so ugly. Pleasing.

--- public/js/Edit.js
+++ public/js/Edit.js
@@ -1,3 +1,103 @@
+
+//Scroll div to bottom on update
+function ScrollDown(){
+  console.log("Attempting to scroll")
+  var mydiv = $('#Preview');
+  mydiv.scrollTop(mydiv.prop('scrollHeight'));
+};
+
+var Preview = {
+  delay: 150,        // delay after keystroke before updating
+
+  preview: null,     // filled in by Init below
+  buffer: null,      // filled in by Init below
+
+  timeout: null,     // store setTimout id
+  mjRunning: false,  // true when MathJax is processing
+  oldText: null,     // used to check if an update is needed
+
+  //
+  //  Get the preview and buffer DIV's
+  //
+  Init: function () {
+    console.log("Initing");
+    this.preview = document.getElementById("Preview");
+    this.buffer = document.getElementById("Buffer");
+  },
+
+  //
+  //  Switch the buffer and preview, and display the right one.
+  //  (We use visibility:hidden rather than display:none since
+  //  the results of running MathJax are more accurate that way.)
+  //
+  SwapBuffers: function () {
+    console.log("Swapping");
+    var buffer = this.preview,
+    preview = this.buffer;
+    this.buffer = buffer; this.preview = preview;
+    buffer.style.visibility = "hidden";
+    buffer.style.position = "absolute";
+    preview.style.visibility = "";
+    preview.style.position = "";
+    //var tmpStyle = this.buffer.style;
+    //this.buffer.style = this.preview.style;
+    //this.preview.style = tmpStyle;
+  },
+
+  //
+  //  This gets called when a key is pressed in the textarea.
+  //  We check if there is already a pending update and clear it if so.
+  //  Then set up an update to occur after a small delay (so if more keys
+  //    are pressed, the update won't occur until after there has been 
+  //    a pause in the typing).
+  //  The callback function is set up below, after the Preview object is set up.
+  //
+  Update: function () {
+    console.log("Updating");
+    if (this.timeout) {clearTimeout(this.timeout)}
+    this.timeout = setTimeout(this.callback,this.delay);
+  },
+
+  //
+  //  Creates the preview and runs MathJax on it.
+  //  If MathJax is already trying to render the code, return
+  //  If the text hasn't changed, return
+  //  Otherwise, indicate that MathJax is running, and start the
+  //    typesetting.  After it is done, call PreviewDone.
+  //  
+  CreatePreview: function () {
+    console.log("Creating Preview");
+    Preview.timeout = null;
+    if (this.mjRunning) return;
+    var text = '<h1>'+$("#title").val()+'</h1>'
+	  +$("#content").val();
+    if (text === this.oldtext) return;
+    this.buffer.innerHTML = this.oldtext = text;
+    this.mjRunning = true;
+    MathJax.Hub.Queue(
+      ["Typeset",MathJax.Hub,this.buffer],
+      ["PreviewDone",this]
+    );
+  },
+
+  //
+  //  Indicate that MathJax is no longer running,
+  //  and swap the buffers to show the results.
+  //
+  PreviewDone: function () {
+    console.log("PreviewDone");
+    this.mjRunning = false;
+    this.SwapBuffers();
+  }
+
+};
+
+//
+//  Cache a callback to the CreatePreview action
+//
+Preview.callback = MathJax.Callback(["CreatePreview",Preview]);
+Preview.callback.autoReset = true;  // make sure it can run more than once
+
 $(document).ready(function() {
   //Override submit action to redirect after
   $("#form1").submit(function() {
@@ -17,24 +117,14 @@ $(document).ready(function() {
   
   //On content change, update mathjax
   $("#content")
-      .bind('input propertychange',function(){
-	$("#Preview").html('<h1>'+$("#title").val()+'</h1>'+$(this).val());
-	MathJax.Hub.Queue(["Typeset",MathJax.Hub,"Preview"]);
-      });
+      .bind('input propertychange',Preview.Update.bind(Preview));
 
   //On title change, update mathjax
   $("#title")
-      .bind('input propertychange',function(){
-	$("#Preview").html(
-	  '<h1>'+$(this).val()+'</h1>'
-	  +$("#content").val()
-	);
-	MathJax.Hub.Queue(["Typeset",MathJax.Hub,"Preview"]);
-      });
+      .bind('input propertychange',Preview.Update.bind(Preview));
+
+  //Set up Preview
+  Preview.Init();
+  Preview.Update();
   
-  //Set up preview on load
-  $("#Preview").html(
-	  '<h1>'+$("#title").val()+'</h1>'
-	  +$("#content").val());
-  MathJax.Hub.Queue(["Typeset",MathJax.Hub,"Preview"]);
 });
